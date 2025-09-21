04040a3bfc53014d56f8bcb41f4e0e4fd8d8712d
Kadeem Austin <dev@kadeemaustin.ai>
2025-09-21 10:37:39 -0400

    Simplify storage client interface.
    This commit does the following:
    - Remove the exists method

--- deepscale/storage/storage_client.py
+++ deepscale/storage/storage_client.py
@@ -4,16 +4,83 @@ from deepscale.run.checkpoint import Checkpoint
 
 
 class StorageClient(Protocol):
-    def init_run(self, run_id: str, train_config: bytes) -> None: ...
+    """Stores and retrieves training artifacts in a storage location.
+
+    This class provides an interface for classes inteded to be used to store and
+    retrieve artifacts (e.g. training configurations, checkpints, etc...) from a
+    configured storage location. (e.g. Amazon S3 bucket).
+    """
+
+    def init_run(self, run_id: str, run_config: bytes) -> None:
+        """Intialize a new training run with the specifed configuration.
+
+        Args:
+            run_id: The unique identifier of the run to be initialized.
+            run_config: The training configuration for this run.
+
+        Raises:
+            StorageError: If an error occurred while attempting to create the training
+                in the storage location.
+        """
+        ...
 
     def resume_run(
-        self, run_id: str, checkpoint_tag: str
-    ) -> tuple[dict[Any, Any], Checkpoint]: ...
+        self, run_id: str, checkpoint_tag: str = "latest"
+    ) -> tuple[dict[Any, Any], Checkpoint | None]:
+        """Load a previous training run.
 
-    def exists(self, run_id: str, checkpoint_tag: str | None = None) -> bool: ...
+        If a checkpoint tag is specified then the checkpoint with the matching tag is
+        returned. If no checkpoint tag is specified then the checkpoint tagged as
+        'latest' will be returned instead.
+
+        If no checkpoint tag is specified and there are also no previously saved
+        checkpoints then none will be returned.
+
+        Args:
+            run_id: The unique identifier of the run to be resumed.
+            checkpoint_tag: The tag of the checkpoing where the run should be resumed
+                from. Defaults to None.
+
+        Returns:
+            Tuple[dict[Any, Any], Checkpoint]:
+                A tuple containing:
+                    - The training configuration used for the specified training run.
+                    - The checkpoint from which the training run should be resumed.
+
+        Raises:
+            RunNotFoundError: If the training run or its configuration cannot be found.
+            CheckpointNotFoundError: If the specified checkpoint could not be found.
+        """
+        ...
 
     def save_checkpoint(
         self, run_id: str, checkpoint_tag: str, checkpoint: bytes
-    ) -> None: ...
+    ) -> None:
+        """Save the specified checkpoint of the given training run.
 
-    def load_checkpoint(self, run_id: str, checkpoint_tag: str) -> bytes: ...
+        Args:
+            run_id: The unique identifier of the training run.
+            checkpoint_tag: The tag to be used for the specified checkpoint.
+            checkpoint: The checkpoint to be saved (in bytes).
+
+        Raises:
+            StorageError: If an error occurred while attempting to save the checkpoint.
+        """
+        ...
+
+    def load_checkpoint(self, run_id: str, checkpoint_tag: str) -> bytes:
+        """Load the specified checkpoint.
+
+        Args:
+            run_id: The unique identifier of the training run for which the checkpoint
+                was saved.
+            checkpoint_tag: The tag of the checkpoint to be loaded.
+
+        Returns:
+            bytes: The matching checkpoint as a raw bytestream.
+
+        Raises:
+            StorageError: If an error occurred while attempting to load the checkpoint.
+            CheckpointNotFoundError: If the specified checkpoint could not be found.
+        """
+        ...
